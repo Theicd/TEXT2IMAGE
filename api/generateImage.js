f9b7b09976d1309f9b9b731d058d888b64a511a7
Theicd <129458154+Theicd@users.noreply.github.com>
2024-11-12 14:44:14 +0200

    Update generateImage.js

--- api/generateImage.js
+++ api/generateImage.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 const { MongoClient } = require('mongodb');
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
     if (req.method !== 'POST') {
         return res.status(405).json({ error: 'Method Not Allowed' });
     }
